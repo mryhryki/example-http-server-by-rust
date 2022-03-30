e2cd8ccebcdd51ff0f7738a1e9643b4d6574ef22
mryhryki <mryhryki@gmail.com>
2022-03-30 18:57:50 +0900

    Copy example code by https://doc.rust-lang.org/std/net/struct.TcpListener.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
+
+fn handle_client(stream: TcpStream) -> () {
+    println!("{:#?}", stream);
 }
