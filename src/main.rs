9e998575261085f8f5713c20aa0ec116fca156ee
mryhryki <mryhryki@gmail.com>
2022-03-30 20:06:13 +0900

    Parse HTTP request headers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::io::Read;
 use std::net::{TcpListener, TcpStream};
+
+struct HttpRequest {
+    method: String,
+    pathname: String,
+    http_version: String,
+}
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
@@ -8,6 +15,18 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn handle_client(stream: TcpStream) -> () {
-    println!("{:#?}", stream);
+fn handle_client(mut stream: TcpStream) {
+    let mut cursor = 0;
+    let mut buf = [0; 1024];
+    let mut len = stream.read(&mut buf).unwrap();
+
+    while len > 0 {
+        for i in 0..len {
+            if buf[i] == 13 && buf[i + 1] == 10 {
+                println!("{:#?}", String::from_utf8(buf[cursor..(i)].to_vec()).unwrap());
+                cursor = i + 2;
+            }
+        }
+        len = stream.read(&mut buf).unwrap();
+    }
 }
