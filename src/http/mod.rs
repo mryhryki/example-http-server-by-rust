0133c8ac9186abdc6b0d28a035ccbaf9040acca8
mryhryki <mryhryki@gmail.com>
2022-04-03 02:30:34 +0900

    Change read stream way and use reuqest and response structs

---
+++ src/http/mod.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct HttpRequest {
+    pub method: String,
+    pub pathname: String,
+    pub http_version: String,
+    pub headers: HashMap<String, String>,
+}
+
+#[derive(Debug)]
+pub struct HttpResponse<'a> {
+    pub status_code: i16,
+    pub status_message: String,
+    pub http_version: String,
+    pub headers: HashMap<String, String>,
+    pub body: Option<&'a [u8]>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,107 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream, Shutdown};
+use crate::http::{HttpRequest, HttpResponse};
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
 
-// struct HttpRequest {
-//     method: String,
-//     pathname: String,
-//     http_version: String,
-// }
+mod http;
 
 fn main() -> std::io::Result<()> {
+    let port = 8080;
     let listener = TcpListener::bind("127.0.0.1:8080")?;
+    println!("HTTP server started, port: {}", port);
+
     for stream in listener.incoming() {
-        handle_client(stream?);
+        let mut stream = stream.unwrap();
+        let response = handle_client(&stream);
+
+        let mut response_buf: Vec<u8> = vec![];
+        response_buf.append(
+            &mut format!(
+                "{} {} {}\r\n",
+                response.http_version, response.status_code, response.status_message
+            )
+            .as_bytes()
+            .to_vec(),
+        );
+        for (key, val) in response.headers {
+            response_buf.append(&mut format!("{}: {}", key, val).as_bytes().to_vec());
+        }
+        match response.body {
+            Some(body) => {
+                response_buf.append(&mut "\r\n".as_bytes().to_vec());
+                response_buf.append(&mut body.to_vec());
+            }
+            None => (),
+        }
+        stream.write(response_buf.as_slice()).unwrap();
     }
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) {
-    let mut cursor = 0;
-    let mut buf = [0; 1024];
-    let mut len = stream.read(&mut buf).unwrap();
+fn handle_client(stream: &TcpStream) -> HttpResponse {
+    let request = parse_request(stream);
 
-    while len > 0 {
-        for i in 0..len {
-            if buf[i] == 13 && buf[i + 1] == 10 {
-                if cursor == i {
-                    stream.write("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset: utf-8\r\n\r\n<h1>OK</h1>".as_bytes()).unwrap();
-                    stream.shutdown(Shutdown::Both).unwrap();
-                    return;
-                }
-                println!("{:#?}", String::from_utf8(buf[cursor..(i)].to_vec()).unwrap());
-                cursor = i + 2;
-            }
+    if request.http_version != "HTTP/1.1" {
+        return HttpResponse {
+            status_code: 505,
+            status_message: String::from("HTTP Version Not Supported"),
+            http_version: request.http_version,
+            headers: HashMap::new(),
+            body: None,
+        };
+    }
+
+    if ![
+        String::from("GET"),
+        String::from("HEAD"),
+        String::from("POST"),
+    ]
+    .contains(&request.method)
+    {
+        return HttpResponse {
+            status_code: 405,
+            status_message: String::from("Method Not Allowed"),
+            http_version: request.http_version,
+            headers: HashMap::new(),
+            body: None,
+        };
+    }
+
+    HttpResponse {
+        status_code: 200,
+        status_message: String::from("OK"),
+        http_version: request.http_version,
+        headers: HashMap::new(),
+        body: Some(b"<h1>Hello, World!</h1>"),
+    }
+}
+
+fn parse_request(stream: &TcpStream) -> HttpRequest {
+    let reader = BufReader::new(stream);
+
+    let mut lines_iter = reader.lines();
+    let mut line = lines_iter.next().unwrap().unwrap();
+
+    let mut arr = line.split(" ");
+    let method = String::from(arr.next().unwrap_or("UNKNOWN"));
+    let pathname = String::from(arr.next().unwrap_or("?"));
+    let http_version = String::from(arr.next().unwrap_or("UNKNOWN_HTTP_VERSION"));
+
+    let mut headers = HashMap::new();
+    while line != "" {
+        line = lines_iter.next().unwrap().unwrap();
+        let mut arr = line.split(":");
+        let key = arr.next().unwrap_or("").trim().to_ascii_lowercase();
+        let val = arr.next().unwrap_or("").trim();
+        if key != "" && val != "" {
+            headers.insert(String::from(key), String::from(val));
         }
-        len = stream.read(&mut buf).unwrap();
+    }
+
+    HttpRequest {
+        method,
+        pathname,
+        http_version,
+        headers,
     }
 }
